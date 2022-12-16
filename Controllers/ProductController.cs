0972d67356f5c05594e749ca4bd3ff6cf2c875c8
Potcharapol Nokyoo <nokink@hotmail.com>
2022-12-17 01:36:48 +0700

    httpGet

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -7,12 +7,42 @@ namespace dotnet_course.Controllers;
 public class ProductController : ControllerBase
 {
     [HttpGet]
-    public ActionResult<List<string>> GetProducts() {
+    // 
+    public ActionResult<List<string>> GetProducts()
+    {
         var products = new List<string>();
         products.Add("ReactJS");
         products.Add("VueJS");
         products.Add("Angular");
 
         return Ok(products);
+    }
+
+    [HttpGet("{id}")]
+    // 
+    public ActionResult<Object> GetProductById(int id)
+    {
+        return Ok(new { productId = id, productName = $"Product {id}" });
+    }
+
+    [HttpGet("search/{id}/{category}")]
+    // 
+    public ActionResult<Object> SearchProductById(int id, string category)
+    {
+        return Ok(new { productId = id, productName = $"Product {id}", category = category });
+    }
+
+    [HttpGet("query")]
+    // 
+    public ActionResult<Object> QueryProductById([FromQuery] int id, [FromQuery] string category)
+    {
+        return Ok(new { productId = id, productName = $"Product {id}", category = category });
+    }
+
+    [HttpGet("query/{user}")]
+    // 
+    public ActionResult<Object> QueryProductByUser([FromQuery] int id, [FromQuery] string category, string user)
+    {
+        return Ok(new { productId = id, productName = $"Product {id}", category = category, user = user });
     }
 }
