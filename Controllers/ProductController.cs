deb5c1ac9a77de09083135be26721ed4efdb09eb
Potcharapol Nokyoo <nokink@hotmail.com>
2022-12-17 14:41:41 +0700

    httpPut

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -7,7 +7,7 @@ namespace dotnet_course.Controllers;
 public class ProductController : ControllerBase
 {
     [HttpGet]
-    // 
+    // ../Product
     public ActionResult<List<string>> GetProducts()
     {
         var products = new List<string>();
@@ -19,30 +19,58 @@ public class ProductController : ControllerBase
     }
 
     [HttpGet("{id}")]
-    // 
+    // ../Product/1
     public ActionResult<Object> GetProductById(int id)
     {
         return Ok(new { productId = id, productName = $"Product {id}" });
     }
 
     [HttpGet("search/{id}/{category}")]
-    // 
+    // ../Product/search/12/tech
     public ActionResult<Object> SearchProductById(int id, string category)
     {
         return Ok(new { productId = id, productName = $"Product {id}", category = category });
     }
 
     [HttpGet("query")]
-    // 
+    // ../Product/query?id=12&category=tech
     public ActionResult<Object> QueryProductById([FromQuery] int id, [FromQuery] string category)
     {
         return Ok(new { productId = id, productName = $"Product {id}", category = category });
     }
 
     [HttpGet("query/{user}")]
-    // 
+    // ../Product/query/inkxk?id=12&category=tech
     public ActionResult<Object> QueryProductByUser([FromQuery] int id, [FromQuery] string category, string user)
     {
         return Ok(new { productId = id, productName = $"Product {id}", category = category, user = user });
+    }
+
+    [HttpPost]
+    public ActionResult<Product> AddProduct([FromBody] Product product)
+    {
+        return Ok(product);
+    }
+
+    public class Product
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public int price { get; set; }
+    }
+
+    // IActionResult ใช้กรณีไม่มี return ค่ากลับ
+    [HttpPut("{id}")]
+    public ActionResult updateProductById(int id, [FromBody] Product product)
+    {
+        if (id != product.id) {
+            return BadRequest();
+        } 
+
+        if (id != 1111) {
+            return NotFound();
+        }
+
+        return Ok(product);
     }
 }
